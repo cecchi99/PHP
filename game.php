98d767efe072d40cb195ccb5b7d0a41d0bcd0bb0
andreacecchi4 <andreacecchi4@gmail.com>
2018-01-31 11:56:31 -0500

    numero random

--- game.php
+++ game.php
@@ -7,24 +7,33 @@
     <h1>Gioco dell'indovina-numero</h1>
     
     <?php
-     $number=30;
-    
      if(isset($_GET["send"])==false)
      {
        $_GET["tentativi"]=1;
+       $_GET["number"]=rand(1,100);
      }
     
-     if(isset($_GET["user_number"]))
+     echo $_GET["number"];
+    
+     if($_GET["tentativi"]==7)
+     {
+       echo "fine tentativi!"."<br/>";
+     }
+     else if(isset($_GET["user_number"]))
      {
        $_GET["tentativi"]++;
        
-       if($_GET["user_number"]>$number)
+       if($_GET["user_number"]>$_GET["number"])
        {
          echo "Il numero è troppo grande!"."<br/>";
        }
-       else if ($_GET["user_number"]<$number)
+       else if ($_GET["user_number"]<$_GET["number"])
        {
          echo "Il numero è troppo piccolo!"."<br/>";
+       }
+       else
+       {
+         echo "indovinato!"."<br/>";
        }
      }
     
@@ -35,6 +44,7 @@
     <form method="get" action="">
       <input type="text" name="user_number">
       <input type="hidden" name="tentativi" value="<?php echo $_GET["tentativi"] ?>">
+      <input type="hidden" name="number" value="<?php echo $_GET["number"] ?>">
       <input type="submit" name="send" value="Conferma">
     </form>
   </body>
