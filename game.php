bde071c1c85d32e4299e3764d8b85c40f22eaa86
andreacecchi4 <andreacecchi4@gmail.com>
2018-02-02 11:21:58 -0500

    gioco funzionante

--- game.php
+++ game.php
@@ -7,13 +7,9 @@
     <h1>Gioco dell'indovina-numero</h1>
     
     <?php
-     if(isset($_GET["send"])==false)
-     {
-       $_GET["tentativi"]=1;
-       $_GET["number"]=rand(1,100);
-     }
-    
-     if($_GET["tentativi"]==7)
+    if(isset($_POST["send"]))
+    {
+     if($_POST["tentativi"]==7)
      {
        ?>
          <h2>Spiacenti...</h2>
@@ -23,37 +19,69 @@
          </form>
        <?php
      }
-     else if(isset($_GET["user_number"]))
+     else 
      {
-       if($_GET["user_number"]>$_GET["number"])
+       if($_POST["user_number"]==$_POST["number"])
        {
-         echo "Il numero e' troppo grande!"."<br/>";
-       }
-       else if ($_GET["user_number"]<$_GET["number"])
-       {
-         echo "Il numero e' troppo piccolo!"."<br/>";
-       }
-       else
-       {?>
+         ?>
          <h2>BRAVO!</h2>
-         <p>Hai indovinato in <?php echo $_GET["tentativi"] ?> tentativi.</p>
+         <p>Hai indovinato in <?php echo $_POST["tentativi"] ?> tentativi.</p>
          <form method="get" action="index.html">
            <input type="submit" name="restart" value="Gioca di nuovo">
          </form>
        <?php
        }
+       else
+       {
+         $_POST["tentativi"]++;
+         
+         if($_POST["user_number"]>$_POST["number"])
+         {
+           echo "Il numero e' troppo grande!"."<br/>";?>
        
-       $_GET["tentativi"]++;
-     }
-    ?>
+           <p>Tentativo n.<?php echo $_POST["tentativi"] ?></p>
+           <b>Inserisci il numero</b>
+           <form method="post" action="">
+            <input type="text" name="user_number">
+            <input type="hidden" name="tentativi" value="<?php echo $_POST["tentativi"] ?>">
+            <input type="hidden" name="number" value="<?php echo $_POST["number"] ?>">
+            <input type="submit" name="send" value="Conferma">
+           </form>
     
-    <p>Tentativo n.<?php echo $_GET["tentativi"] ?></p>
-    <b>Inserisci il numero</b>
-    <form method="get" action="">
+    <?php }
+       else if ($_POST["user_number"]<$_POST["number"])
+       {
+         echo "Il numero e' troppo piccolo!"."<br/>";?>
+       
+         <p>Tentativo n.<?php echo $_POST["tentativi"] ?></p>
+         <b>Inserisci il numero</b>
+         <form method="post" action="">
+          <input type="text" name="user_number">
+          <input type="hidden" name="tentativi" value="<?php echo $_POST["tentativi"] ?>">
+          <input type="hidden" name="number" value="<?php echo $_POST["number"] ?>">
+          <input type="submit" name="send" value="Conferma">
+         </form>
+    
+    <?php } 
+       }
+     }
+    }
+    else
+     {
+       $_POST["tentativi"]=1;
+       $_POST["number"]=rand(1,100);?>
+    
+       <p>Tentativo n.<?php echo $_POST["tentativi"] ?></p>
+       <b>Inserisci il numero</b>
+    <form method="post" action="">
       <input type="text" name="user_number">
-      <input type="hidden" name="tentativi" value="<?php echo $_GET["tentativi"] ?>">
-      <input type="hidden" name="number" value="<?php echo $_GET["number"] ?>">
+      <input type="hidden" name="tentativi" value="<?php echo $_POST["tentativi"] ?>">
+      <input type="hidden" name="number" value="<?php echo $_POST["number"] ?>">
       <input type="submit" name="send" value="Conferma">
     </form>
+    
+    <?php }
+    ?>
+   
   </body>
 </html>
