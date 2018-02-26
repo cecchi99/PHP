0c0afa369a01eab4e386da08c86624425da9e1be
andreacecchi4 <andreacecchi4@gmail.com>
2018-02-26 03:07:00 -0500

    login

--- user.php
+++ user.php
@@ -21,12 +21,50 @@
     
     <h1>Modulo di accesso</h1>
     
+    <?php
+    if(isset($_POST["login"]))
+    {
+      $connection="mysql:host=localhost;dbname=quintab_cecchi";
+      $username="root";
+      $password="quintab";
+      
+      try
+      {
+        $dbh=new PDO($connection,$username,$password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        
+        $query=$dbh->prepare("SELECT email, password FROM iscrizione WHERE email=:email AND password=:password");
+        $query->bindValue(":email",$_POST["email"]);
+        $query->bindValue(":password",md5($_POST["password"]));
+        
+        $query->execute();
+        
+        if($query->fetch()>0)
+        {
+          echo "Accesso eseguito con: ".$_POST["email"]."<br>";
+        }
+        else
+        {
+          echo "Credenziali non valide!"."<br>";
+        }
+      }
+      catch(PDOException $e)
+      {
+        echo $e->getMessage()."<br>";
+      }
+    }
+    else
+    {
+    ?>
+    
     <p>Hai gia' un account?</p>
-    <form name="loginform" method="post" action="index.php" onsubmit="return isEmpty()">
+    <form name="loginform" method="post" action="" onsubmit="return isEmpty()">
       e-Mail: <input type="email" name="email"><br>
       Password: <input type="password" name="password"><br>
       <input type="submit" name="login" value="Accedi">
+      <?php } ?>
       <input type="button" name="return" value="Indietro" onclick="goBack()">
     </form>
+    
   </body>
 </html>
