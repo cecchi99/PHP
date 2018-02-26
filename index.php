dc4668c4beb56d2a447eca6460590d694c43d78a
andreacecchi4 <andreacecchi4@gmail.com>
2018-02-26 03:30:00 -0500

    logout

--- index.php
+++ index.php
@@ -14,6 +14,6 @@ if(isset($_SESSION["email"]))
     <h1>Home</h1>
     
     <a href="register.php">Registrati</a><br>
-    <a href="user.php">Accedi</a>
+    <a href="login.php">Accedi</a>
   </body>
 </html>
