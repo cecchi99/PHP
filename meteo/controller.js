0f00ec2830cbf50c715d7b10fe659a95578135f9
andreacecchi4 <andreacecchi4@gmail.com>
2018-04-25 10:10:12 -0400

    tabella meteo e cancellazione

--- meteo/controller.js
+++ meteo/controller.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
     $.getJSON("http://api.openweathermap.org/data/2.5/weather",{'q':$("#loc1").val(),'APPID':"08f26442f35c69050b5dc94377b4dc7f"},function (result){
      
       //variabili tabella
-      var loc="<td><th>"+result.name+", "+result.sys.country+"</th><tr>(lon="+result.coord.lon+", lat="+result.coord.lat+")</tr></td>";
-      var main="<td><tr>Clima:</tr><tr>"+result.weather[0].main+"</tr></td>";
-      var dsc="<td><tr>Descrizione:</tr><tr>"+result.weather[0].description+"</tr></td>";
-      var tmp="<td><tr>Temperatura:</tr><tr>"+result.main.temp+"°C</tr></td>";
-      var pr="<td><tr>Pressione:</tr><tr>"+result.main.pressure+" hpa</tr></td>";
-      var hum="<td><tr>Umidita':</tr><tr>"+result.main.humidity+"%</tr></td>";
-      var spd="<td><tr>Velocita' vento:</tr><tr>"+result.wind.speed+" m/s</tr></td>";
-      var deg="<td><tr>Direzione vento:</tr><tr>"+result.wind.deg+"°</tr></td>";
-      var vis="<td><tr>Visibilita':</tr><tr>"+result.visibility+"</tr></td>";
+      var loc="<tr><th>"+result.name+", "+result.sys.country+"</th><td>(lon="+result.coord.lon+", lat="+result.coord.lat+")</td></tr>";
+      var main="<tr><td>Clima:</td><td>"+result.weather[0].main+"</td></tr>";
+      var dsc="<tr><td>Descrizione:</td><td>"+result.weather[0].description+"</td></tr>";
+      var tmp="<tr><td>Temperatura:</td><td>"+result.main.temp+"°C</td></tr>";
+      var pr="<tr><td>Pressione:</td><td>"+result.main.pressure+" hpa</td></tr>";
+      var hum="<tr><td>Umidita':</td><td>"+result.main.humidity+"%</td></tr>";
+      var spd="<tr><td>Velocita' vento:</td><td>"+result.wind.speed+" m/s</td></tr>";
+      var deg="<tr><td>Direzione vento:</td><td>"+result.wind.deg+"°</td></tr>";
+      var vis="<tr><td>Visibilita':</td><td>"+result.visibility+"</td></tr>";
       
       //valori in tabella
       var tb=loc+main+dsc+tmp+pr+hum+spd+deg+vis;
@@ -22,6 +22,12 @@ $(document).ready(function(){
     }).fail(function(){
       alert("Localita' inesistente!");
     });
+  });
+  
+  //cancellazione dati meteo
+  $("#delete1").click(function(){
+    $("#meteo").empty();
+    $("#loc1").val("");
   });
   
 });
