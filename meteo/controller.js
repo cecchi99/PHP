758e9c3d81253718dc432d33a0dcc2e7902d8553
andreacecchi4 <andreacecchi4@gmail.com>
2018-05-10 07:36:46 -0400

    utilizzo funzioni

--- meteo/controller.js
+++ meteo/controller.js
@@ -1,35 +1,16 @@
-//previsioni per data e ora
+//variabile dei dati ricevuti
+var data;
+
+//funzione di cambio valori filtrati con la select
 function Change(sel)
 {
-  $.getJSON("http://api.openweathermap.org/data/2.5/forecast",{'q':$("#loc2").val(),'APPID':"08f26442f35c69050b5dc94377b4dc7f"},function (result){
-     
-      //svuotamento tabella precedente
-      $("#previsioni").empty();
-      
-      //variabili tabella della option datetime selezionata
-      var loc="<tr><th>"+result.city.name+", "+result.city.country+"</th><td>(lon="+result.city.coord.lon+", lat="+result.city.coord.lat+")</td></tr>";
-      var main="<tr><td>Clima:</td><td>"+result.list[sel.value].weather[0].main+"</td></tr>";
-      var dsc="<tr><td>Descrizione:</td><td>"+result.list[sel.value].weather[0].description+"</td></tr>";
-      var tmp="<tr><td>Temperatura:</td><td>"+result.list[sel.value].main.temp+"°C</td></tr>";
-      var pr="<tr><td>Pressione:</td><td>"+result.list[sel.value].main.pressure+" hpa</td></tr>";
-      var hum="<tr><td>Umidita':</td><td>"+result.list[sel.value].main.humidity+"%</td></tr>";
-      var spd="<tr><td>Velocita' vento:</td><td>"+result.list[sel.value].wind.speed+" m/s</td></tr>";
-      var deg="<tr><td>Direzione vento:</td><td>"+result.list[sel.value].wind.deg+"°</td></tr>";
-      
-      //valori in tabella aggiornata
-      var tb=loc+main+dsc+tmp+pr+hum+spd+deg;
-      $("#previsioni").append(tb);
-      
-    }).fail(function(){
-      alert("Localita' inesistente!");
-    });
+    Forecast(sel.value,data);
 }
 
-$(document).ready(function(){
-  
-  //dati meteo tramite località
-  $("#search1").click(function(){  
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather",{'q':$("#loc1").val(),'APPID':"08f26442f35c69050b5dc94377b4dc7f"},function (result){
+//funzione dati meteo
+function Weather()
+{
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather",{'q':$("#loc1").val(),'APPID':"08f26442f35c69050b5dc94377b4dc7f"},function (result){
       
       //svuotamento tabella precedente
       $("#meteo").empty();
@@ -38,11 +19,11 @@ $(document).ready(function(){
       var loc="<tr><th>"+result.name+", "+result.sys.country+"</th><td>(lon="+result.coord.lon+", lat="+result.coord.lat+")</td></tr>";
       var main="<tr><td>Clima:</td><td>"+result.weather[0].main+"</td></tr>";
       var dsc="<tr><td>Descrizione:</td><td>"+result.weather[0].description+"</td></tr>";
-      var tmp="<tr><td>Temperatura:</td><td>"+result.main.temp+"°C</td></tr>";
+      var tmp="<tr><td>Temperatura:</td><td>"+result.main.temp+"&deg;C</td></tr>";
       var pr="<tr><td>Pressione:</td><td>"+result.main.pressure+" hpa</td></tr>";
       var hum="<tr><td>Umidita':</td><td>"+result.main.humidity+"%</td></tr>";
       var spd="<tr><td>Velocita' vento:</td><td>"+result.wind.speed+" m/s</td></tr>";
-      var deg="<tr><td>Direzione vento:</td><td>"+result.wind.deg+"°</td></tr>";
+      var deg="<tr><td>Direzione vento:</td><td>"+result.wind.deg+"&deg;</td></tr>";
       
       //valori in tabella
       var tb=loc+main+dsc+tmp+pr+hum+spd+deg;
@@ -51,7 +32,31 @@ $(document).ready(function(){
     }).fail(function(){
       alert("Localita' inesistente!");
     });
-  });
+}
+
+//cambio dati meteo per data e ora
+function Forecast(n,result)
+{
+      //svuotamento tabella precedente
+      $("#previsioni").empty();
+      
+      //variabili tabella della option datetime selezionata
+      var loc="<tr><th>"+result.city.name+", "+result.city.country+"</th><td>(lon="+result.city.coord.lon+", lat="+result.city.coord.lat+")</td></tr>";
+      var main="<tr><td>Clima:</td><td>"+result.list[n].weather[0].main+"</td></tr>";
+      var dsc="<tr><td>Descrizione:</td><td>"+result.list[n].weather[0].description+"</td></tr>";
+      var tmp="<tr><td>Temperatura:</td><td>"+result.list[n].main.temp+"&deg;C</td></tr>";
+      var pr="<tr><td>Pressione:</td><td>"+result.list[n].main.pressure+" hpa</td></tr>";
+      var hum="<tr><td>Umidita':</td><td>"+result.list[n].main.humidity+"%</td></tr>";
+      var spd="<tr><td>Velocita' vento:</td><td>"+result.list[n].wind.speed+" m/s</td></tr>";
+      var deg="<tr><td>Direzione vento:</td><td>"+result.list[n].wind.deg+"&deg;</td></tr>";
+      
+      //valori in tabella aggiornata
+      var tb=loc+main+dsc+tmp+pr+hum+spd+deg;
+      $("#previsioni").append(tb);
+}
+
+//funzione principale del documento
+$(document).ready(function(){
   
   //cancellazione dati meteo
   $("#delete1").click(function(){
@@ -60,28 +65,16 @@ $(document).ready(function(){
   });
   
   //dati previsioni tramite località
-  $("#search2").click(function(){  
+  $("#search2").click(function(){
+    
+    //chiamata al database
     $.getJSON("http://api.openweathermap.org/data/2.5/forecast",{'q':$("#loc2").val(),'APPID':"08f26442f35c69050b5dc94377b4dc7f"},function (result){
      
       //mostra select
       $("#datetime").show();
       
-      //svuotamento tabella precedente
-      $("#previsioni").empty();
-      
-      //variabili tabella
-      var loc="<tr><th>"+result.city.name+", "+result.city.country+"</th><td>(lon="+result.city.coord.lon+", lat="+result.city.coord.lat+")</td></tr>";
-      var main="<tr><td>Clima:</td><td>"+result.list[0].weather[0].main+"</td></tr>";
-      var dsc="<tr><td>Descrizione:</td><td>"+result.list[0].weather[0].description+"</td></tr>";
-      var tmp="<tr><td>Temperatura:</td><td>"+result.list[0].main.temp+"°C</td></tr>";
-      var pr="<tr><td>Pressione:</td><td>"+result.list[0].main.pressure+" hpa</td></tr>";
-      var hum="<tr><td>Umidita':</td><td>"+result.list[0].main.humidity+"%</td></tr>";
-      var spd="<tr><td>Velocita' vento:</td><td>"+result.list[0].wind.speed+" m/s</td></tr>";
-      var deg="<tr><td>Direzione vento:</td><td>"+result.list[0].wind.deg+"°</td></tr>";
-      
-      //valori in tabella
-      var tb=loc+main+dsc+tmp+pr+hum+spd+deg;
-      $("#previsioni").append(tb);
+      data=result;
+      Forecast(0,data);
       
       //svuotamento select precedente
       $("#datetime").empty();
